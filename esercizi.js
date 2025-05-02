aefff098732d17d31c3904d160ccb367237ddf2f
Riccardo-Gucciardo <riccardogucciardo003@gmail.com>
2025-05-02 18:00:36 +0200

    snack 2

--- esercizi.js
+++ esercizi.js
@@ -1,7 +1,8 @@
-function somma(a, b) {
-    return a + b;
-}
+// function somma(a, b) {
+//     return a + b;
+// }
 
+// Test della funzione
 // console.log(somma(2,3))
 
 // const sommanonima = function(a,b){
@@ -10,6 +11,13 @@ function somma(a, b) {
 
 
 
-const sommanonima = (a,b) => a + b ;
+// const sommanonima = (a,b) => a + b ;
 
-console.log(sommanonima(1,2))
+// Test della funzione
+// console.log(sommanonima(1,2))
+
+
+function quadrato(numero) { return numero * numero; }
+
+// Test della funzione
+// console.log(quadrato(4));
